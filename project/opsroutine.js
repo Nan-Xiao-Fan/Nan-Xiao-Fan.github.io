024f0444a1c1b2f50b1274e6a6fe128ea6866e63
Wenrui Huang <rijuyuezhu@gmail.com>
2023-08-19 12:30:39 +0800

    Deployed 76de080 with MkDocs version: 1.4.3

---
+++ project/opsroutine.js
@@ -0,0 +1,188 @@
+const displayWindow = document.getElementById('displayWindow');
+const operatePane = document.getElementById('operatePane');
+const operations = document.getElementById('operations');
+const N = 9;
+let boxs = [];
+let selectBox = [];
+let isFull = [];
+let isLock = [];
+let takeaway = [];
+let nowSelect = -1;
+const beginWindow = () => {
+	const beginWindow = document.getElementById('beginWindow');
+	beginWindow.style.display = 'none';
+	displayWindow.style.display = 'block';
+	boxs = [];
+	selectBox = [];
+	isFull = [];
+	isLock = [];
+	takeaway = [];
+	nowSelect = -1;
+	gameInit();
+}
+const gameInit = () => {
+	for (let i = 0; i < N; i++) {
+		let box = document.createElement('button');
+		box.type = 'button';
+		box.classList.add('box');
+		box.innerHTML = "<p>" + (i+1).toString() + "</p>"
+		box.onclick = () => {clickBox(i);}
+		boxs.push(box);
+		selectBox.push(0);
+		isFull.push(0);
+		isLock.push(0);
+		takeaway.push(new Array);
+		displayWindow.appendChild(box);
+	}
+}
+const getBox = (i) => {
+	boxs[i].style.borderColor = 'red';
+}
+const ungetBox = (i) => {
+	boxs[i].style.borderColor = '';
+
+}
+const clickBox = (i) => {
+	selectBox[i] ^= 1;
+	if (selectBox[i]) {
+		if (nowSelect !== -1) {
+			selectBox[nowSelect] = 0;
+			ungetBox(nowSelect);
+		}
+		getBox(i);
+		nowSelect = i;
+	} else {
+		ungetBox(i);
+		nowSelect = -1;
+	}
+	showOperatePane();
+}
+const changeBoxStatus = (i) => {
+	const subh = boxs[i].firstChild;
+	if (isFull[i]) {
+		if (isLock[i]) {
+			subh.style.color = 'white';
+			boxs[i].style.backgroundColor = 'green';
+		}
+		else {
+			subh.style.color = 'white';
+			boxs[i].style.backgroundColor = 'blue';
+		}
+	} else {
+		if (isLock[i]) {
+			subh.style.color = 'white';
+			boxs[i].style.backgroundColor = 'brown';
+		}
+		else {
+			subh.style.color = '';
+			boxs[i].style.backgroundColor = '';
+		}
+	}
+}
+
+const showOperatePane = () => {
+	if (nowSelect === -1) {
+		operatePane.style.display = 'none';
+	} else {
+		operatePane.style.display = 'block';
+		showOperations();
+	}
+}
+
+const cfullButton = () => {
+	const x = nowSelect;
+	if (!isFull[x] && isLock[x])
+		return;
+	const cfull = document.createElement('button');
+	cfull.type = 'button';
+	cfull.classList.add('defaultButton');
+	if (isFull[x]) {
+		cfull.innerHTML = '<p>' + (isLock[x] ? '解锁' : '') + '取餐</p>';
+		cfull.onclick = () => {
+			if (isLock[x]) {
+				const answer = prompt('取 ' + (x+1).toString()+ ' 号外卖。\n学生请在终端机上刷卡选择解锁；\n社会人士请联系保安登记取走。\n你的标识号为：');
+				if (answer === null) {
+					return ;
+				}
+				takeaway[x].push(answer);
+			}
+			isFull[x] = isLock[x] = 0;
+			changeBoxStatus(x);
+			showOperatePane();
+		}
+	} else {
+		cfull.innerHTML = '<p>放餐</p>';
+		cfull.onclick = () => {
+			isFull[x] = 1;
+			changeBoxStatus(x);
+			showOperatePane();
+		}
+	}
+	operations.appendChild(cfull);
+}
+const clockButton = () => {
+	const x = nowSelect;
+	if (isFull[x] && isLock[x])
+		return;
+	const clock = document.createElement('button');
+	clock.type = 'button';
+	clock.classList.add('defaultButton');
+	if (!isLock[x]) {
+		clock.innerHTML = '<p>上锁</p>';
+		clock.onclick = () => {
+			isLock[x] = 1;
+			changeBoxStatus(x);
+			showOperatePane();
+		}
+	} else {
+		clock.innerHTML = '<p>（管理员）处理空锁</p>';
+		clock.onclick = () => {
+			isLock[x] = 0;
+			changeBoxStatus(x);
+			showOperatePane();
+		}
+	}
+	operations.appendChild(clock);
+}
+const cstealButton = () => {
+	const x = nowSelect;
+	const csteal = document.createElement('button');
+	csteal.type = 'button';
+	csteal.classList.add('defaultButton');
+	csteal.innerHTML = '<p>外卖被偷举报</p>';
+	csteal.onclick = () => {
+		if (takeaway[x].length === 0) {
+			alert('在信息化中心查询...\n第 ' + (x+1).toString() + ' 号外卖锁过去无外卖取出');
+		} else {
+			let ret = '在信息化中心查询...\n第 ' + (x+1).toString() + ' 号外卖锁过去共有 ' + takeaway[x].length.toString() + ' 人取出外卖：\n';
+			for (let i = 0; i < takeaway[x].length; i++) {
+				ret += (i+1).toString() + ': ' + takeaway[x][i] + '\n';
+			}
+			alert(ret);
+
+		}
+		showOperatePane();
+	}
+	operations.appendChild(csteal);
+}
+const crobButton = () => {
+	const x = nowSelect;
+	if (!isFull[x] || !isLock[x])
+		return;
+	const crob = document.createElement('button');
+	crob.type = 'button';
+	crob.classList.add('defaultButton');
+	crob.innerHTML = '<p>强行扯袋取走</p>';
+	crob.onclick = () => {
+		alert('动作过大，被保安扭送带走。');
+		showOperatePane();
+	}
+	operations.appendChild(crob);
+}
+const showOperations = () => {
+	operations.innerHTML = '';
+	cfullButton();
+	clockButton();
+	cstealButton();
+	crobButton();
+}
