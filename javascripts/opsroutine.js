634ec6dd62abfbedb36982ce23afd7d35668a03b
Wenrui Huang <rijuyuezhu@gmail.com>
2023-08-19 16:06:36 +0800

    Deployed a35bc54 with MkDocs version: 1.4.3

--- javascripts/opsroutine.js
+++ javascripts/opsroutine.js
@@ -10,14 +10,11 @@ let takeaway = [];
 let nowSelect = -1;
 const beginWindow = () => {
 	const beginWindow = document.getElementById('beginWindow');
+	const promptWindow = document.getElementById('promptWindow');
 	beginWindow.style.display = 'none';
+	promptWindow.style.display = 'none';
 	displayWindow.style.display = 'block';
-	boxs = [];
-	selectBox = [];
-	isFull = [];
-	isLock = [];
-	takeaway = [];
-	nowSelect = -1;
+	
 	gameInit();
 }
 const gameInit = () => {
@@ -25,7 +22,7 @@ const gameInit = () => {
 		let box = document.createElement('button');
 		box.type = 'button';
 		box.classList.add('box');
-		box.innerHTML = "<p>" + (i+1).toString() + "</p>"
+		box.innerHTML = "<span>" + (i+1).toString() + "</span>"
 		box.onclick = () => {clickBox(i);}
 		boxs.push(box);
 		selectBox.push(0);
@@ -97,7 +94,7 @@ const cfullButton = () => {
 	cfull.type = 'button';
 	cfull.classList.add('defaultButton');
 	if (isFull[x]) {
-		cfull.innerHTML = '<p>' + (isLock[x] ? '解锁' : '') + '取餐</p>';
+		cfull.innerHTML = '<span>' + (isLock[x] ? '解锁' : '') + '取餐</span>';
 		cfull.onclick = () => {
 			if (isLock[x]) {
 				const answer = prompt('取 ' + (x+1).toString()+ ' 号外卖。\n学生请在终端机上刷卡选择解锁；\n社会人士请联系保安登记取走。\n你的标识号为：');
@@ -111,7 +108,7 @@ const cfullButton = () => {
 			showOperatePane();
 		}
 	} else {
-		cfull.innerHTML = '<p>放餐</p>';
+		cfull.innerHTML = '<span>放餐</span>';
 		cfull.onclick = () => {
 			isFull[x] = 1;
 			changeBoxStatus(x);
@@ -128,14 +125,14 @@ const clockButton = () => {
 	clock.type = 'button';
 	clock.classList.add('defaultButton');
 	if (!isLock[x]) {
-		clock.innerHTML = '<p>上锁</p>';
+		clock.innerHTML = '<span>上锁</span>';
 		clock.onclick = () => {
 			isLock[x] = 1;
 			changeBoxStatus(x);
 			showOperatePane();
 		}
 	} else {
-		clock.innerHTML = '<p>（管理员）处理空锁</p>';
+		clock.innerHTML = '<span>（管理员）处理空锁</span>';
 		clock.onclick = () => {
 			isLock[x] = 0;
 			changeBoxStatus(x);
@@ -149,7 +146,7 @@ const cstealButton = () => {
 	const csteal = document.createElement('button');
 	csteal.type = 'button';
 	csteal.classList.add('defaultButton');
-	csteal.innerHTML = '<p>外卖被偷举报</p>';
+	csteal.innerHTML = '<span>外卖被偷举报</span>';
 	csteal.onclick = () => {
 		if (takeaway[x].length === 0) {
 			alert('在信息化中心查询...\n第 ' + (x+1).toString() + ' 号外卖锁过去无外卖取出');
@@ -172,7 +169,7 @@ const crobButton = () => {
 	const crob = document.createElement('button');
 	crob.type = 'button';
 	crob.classList.add('defaultButton');
-	crob.innerHTML = '<p>强行扯袋取走</p>';
+	crob.innerHTML = '<span>强行扯袋取走</span>';
 	crob.onclick = () => {
 		alert('动作过大，被保安扭送带走。');
 		showOperatePane();
